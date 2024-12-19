da03e81f36c7947de9785a8fde6d37f80faa2be7
urish11 <117926571+urish11@users.noreply.github.com>
2024-12-19 11:05:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def get_representative_phrase(keywords):
 
 def aggregate_by_cluster(data, cluster_data):
     data = data.merge(cluster_data, left_on="Keyword", right_on="Keyword")
-     print("Columns after merge:", data.columns)
+    print("Columns after merge:", data.columns)
 
 
     # Calculate weighted means for relevant columns
