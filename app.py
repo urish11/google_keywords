36139c9dbc2bdcba09b93f56e4cb236ebe9bd50b
urish11 <117926571+urish11@users.noreply.github.com>
2024-12-19 11:50:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def fetch_keyword_data(keyword, location_id, language_id):
           
           return pd.DataFrame(keywords_data)
 
-         except GoogleAdsException as ex:
+          except GoogleAdsException as ex:
              st.error(f"Error fetching data for keyword '{keyword}': Check your API credentials and parameters.")
              return pd.DataFrame()
     except:
