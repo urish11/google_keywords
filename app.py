7e752580901bac976a2903b9db082b6220949bc7
urish11 <uri1234@gmail.com>
2025-05-26 21:23:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def fetch_keyword_data(keyword, location_id, language_id , network):
             request.keyword_plan_network = client.enums.KeywordPlanNetworkEnum.GOOGLE_SEARCH
         
         keyword_seed = client.get_type("KeywordSeed")
-        keyword_seed.keywords.extend([keyword])
+        keyword_seed.keywords.extend(keyword)
         request.keyword_seed = keyword_seed
 
         response = keyword_plan_idea_service.generate_keyword_ideas(request=request)
@@ -331,12 +331,12 @@ if st.button("Fetch Keyword Ideas"):
         else:
             all_data = pd.DataFrame()
             st.text("going to google")
-            for keyword in keywords:
-                for network in ["GOOGLE_SEARCH_AND_PARTNERS", "GOOGLE_SEARCH"]:
-                    data = fetch_keyword_data(keyword, selected_location, selected_language,network)
-                    time.sleep(1)
+            
+            for network in ["GOOGLE_SEARCH_AND_PARTNERS", "GOOGLE_SEARCH"]:
+                data = fetch_keyword_data(keywords, selected_location, selected_language,network)
+                time.sleep(1)
 
-                    all_data = pd.concat([all_data, data], ignore_index=True)
+                all_data = pd.concat([all_data, data], ignore_index=True)
             st.text("done")
             # st.text(all_data)
             all_data = get_network_delta(all_data)
