1144789a298c94d22471ef3c37f5056f8465e2f6
urish11 <uri1234@gmail.com>
2025-05-26 21:11:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -334,6 +334,8 @@ if st.button("Fetch Keyword Ideas"):
             for keyword in keywords:
                 for network in ["GOOGLE_SEARCH_AND_PARTNERS", "GOOGLE_SEARCH"]:
                     data = fetch_keyword_data(keyword, selected_location, selected_language,network)
+                    time.sleep(1)
+
                     all_data = pd.concat([all_data, data], ignore_index=True)
             st.text("done")
             # st.text(all_data)
