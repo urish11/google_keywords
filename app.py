afe01b82fc99a1ed7a7d17e56d932249b6f243b4
urish11 <uri1234@gmail.com>
2025-05-26 22:58:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -332,7 +332,7 @@ if st.button("Fetch Keyword Ideas"):
             all_data = pd.DataFrame()
             st.text("going to google")
             n_of_chunks =len(keywords)// 20 + 1
-            if len(keywords) > 19 : n_of_chunks=1
+            if len(keywords) > 20 : n_of_chunks=1
             # n_of_chunks= len(keywords)
 
             st.text(type(keywords))
