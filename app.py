751595cc8694d4a9f9072cf63f26f47b9514cfa8
urish11 <uri1234@gmail.com>
2025-05-27 16:34:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -420,7 +420,7 @@ if "all_data" in st.session_state:
     grid_options['paginationPageSize'] = 50
     grid_options['pagination'] = True
     grid_options['paginationAutoPageSize'] = False
-    grid_options['paginationPageSizeOptions'] = [10, 20, 50, 100, 200,400,,500,600,700,800,900,1000,2000]  # 👈 Here are your presets
+    grid_options['paginationPageSizeOptions'] = [10, 20, 50, 100, 200,400,500,600,700,800,900,1000,2000]  # 👈 Here are your presets
     grid_options['suppressPaginationPanel'] = False  # Must be False to show the dropdown
 
     grid_response = AgGrid(all_data, gridOptions=grid_options, height=800, width=700, theme="streamlit",update_mode='SELECTION_CHANGED')
